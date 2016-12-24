d3cc35873dc1b5c430cd57e36b4c204bd3cc629a
nyansama <nyansamsa@163.com>
2016-12-24 15:27:23 +0800

    add class data

--- bilibili/list/DEF_DATA.py
+++ bilibili/list/DEF_DATA.py
@@ -10,7 +10,43 @@
 """
 
 
-class types():
+class data:
+    def __init__(self):
+        self.id = -2
+        self.name = ""
+        self.favorite = -2
+        self.year = -2
+        self.season = -2
+        self.pubtime = -2
+        self.status = -2
+        self.total_count = -2
+        self.updatetime = -2
+        self.week = -2
+        self.country = -2
+        self.types = -2
+        self.url = ""
+        self.cover = ""
+
+    def __str__(self):
+        data_list = []
+        data_list.append(str(self.id))
+        data_list.append(self.name)
+        data_list.append(str(self.favorite))
+        data_list.append(str(self.year))
+        data_list.append(str(self.season))
+        data_list.append(str(self.pubtime))
+        data_list.append(str(self.status))
+        data_list.append(str(self.total_count))
+        data_list.append(str(self.updatetime))
+        data_list.append(str(self.week))
+        data_list.append(str(self.country))
+        data_list.append(str(self.types))
+        data_list.append(self.url)
+        data_list.append(self.cover)
+        return ",".join(data_list)
+
+
+class types:
     def __init__(self):
         # TYPE NUM DEFINE
         # global TYPE_TV, TYPE_ALL, TYPE_OVA, TYPE_THEME, TYPE_ORTHER
